fb84d61832f404947be8509c77601c90ecaf4532
Rafał Książek <rafal@amsterdam-standard.pl>
2018-11-10 14:31:12 +0100

    Remove password in user after login

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -5,7 +5,7 @@ export default class AuthController {
     login(request, response, next) {
         const { email, password } = request.body;
 
-        User.findOne({ where: { email }, attributes: ['id', 'password'] })
+        User.findOne({ where: { email }, attributes: ['id', 'username', 'first_name', 'last_name', 'email', 'password'] })
             .then(user => {
                 if (!user) {
                     return response.status(401).json({ auth: false, token: null });
@@ -13,6 +13,7 @@ export default class AuthController {
 
                 if (Auth.checkCredentials(password, user.getDataValue('password'))) {
                     const token = Auth.signIn(user);
+                    delete user.password;
 
                     return response.json({ auth: true, token, user });
                 }
