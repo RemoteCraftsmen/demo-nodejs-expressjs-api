b3741ff19e4e849e351ecb3541e90087932dde4e
Rafał Książek <rafal@amsterdam-standard.pl>
2018-11-14 17:36:38 +0100

    Fixing problem with no-reply, and fixing forgot-password link

--- app/emails/ResetPassword.js
+++ app/emails/ResetPassword.js
@@ -4,9 +4,10 @@ const config = require('../../config/config.json')[env];
 export default function(data) {
     const { email = 'catch@remotecraftsmen.com', first_name = 'John', last_name = 'Doe', token = 'nope' } = data;
     const FRONTEND_URL = config.frontendUrl;
+    const NO_REPLY_ADDRESS = config.noReplyAddress;
 
     return {
-        from: '"Admin" <no-reply@remotecraftsmen.com>',
+        from: `"no-reply" <${NO_REPLY_ADDRESS}>`,
         to: email,
         subject: 'Password reset',
         text: `
@@ -16,7 +17,7 @@ export default function(data) {
 
             If it was not you, please ignore this email.
 
-            To change your password go to this url: ${FRONTEND_URL}/reset-password/${token}
+            To change your password go to this url: ${FRONTEND_URL}/forgot-password/${token}
 
             Thanks!
         `,
@@ -29,7 +30,7 @@ export default function(data) {
                 <strong>If it was not you, please ignore this email.</strong>
             </p>
             <p>
-                To change your password go to this url <a href="${FRONTEND_URL}/reset-password/${token}">${FRONTEND_URL}/change-password/${token}</a>
+                To change your password go to this url <a href="${FRONTEND_URL}/forgot-password/${token}">${FRONTEND_URL}/forgot-password/${token}</a>
             </p>
             <p>
                 Thanks!
