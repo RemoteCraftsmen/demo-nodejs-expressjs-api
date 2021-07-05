93d8ca1d8eb6e3b7c92473ff765d894a659289e9
Peter <piotr.major@remotecraftsmen.com>
2021-07-05 15:43:14 +0200

    added prettier

--- app/seeders/dev/20181029133333-todos.js
+++ app/seeders/dev/20181029133333-todos.js
@@ -1,26 +1,26 @@
-const faker = require("faker");
+const faker = require('faker');
 
 const todos = [];
 
 for (let i = 1; i <= 10; i++) {
-  const todo = {
-    name: faker.internet.domainWord(),
-    completed: false,
-    user_id: i,
-    creator_id: i,
-    created_at: new Date(),
-    updated_at: new Date(),
-  };
+    const todo = {
+        name: faker.internet.domainWord(),
+        completed: false,
+        user_id: i,
+        creator_id: i,
+        created_at: new Date(),
+        updated_at: new Date()
+    };
 
-  todos.push(todo);
+    todos.push(todo);
 }
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert("Todos", todos);
-  },
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('Todos', todos);
+    },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete("Todos");
-  },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('Todos');
+    }
 };
