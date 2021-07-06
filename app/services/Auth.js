e12d86c92e48003e89df38df288d9cba98feae31
Peter <piotr.major@remotecraftsmen.com>
2021-07-06 11:30:29 +0200

    fixed

--- app/services/Auth.js
+++ app/services/Auth.js
@@ -4,10 +4,12 @@ const config = require('../../config');
 
 class Auth {
     static signIn(user) {
-        return jwt.sign({id: user.getDataValue('id')}, config.jwt.secret, {expiresIn: 60 * 60 * 24});
+        return jwt.sign({ id: user.getDataValue('id') }, config.jwt.secret, {
+            expiresIn: 60 * 60 * 24
+        });
     }
 
-    static checkCredentials(password, dbPassword) {
+    static comparePasswords(password, dbPassword) {
         return bcrypt.compareSync(password, dbPassword);
     }
 }
