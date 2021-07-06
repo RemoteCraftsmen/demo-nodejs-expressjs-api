3670aaa9fe07b73425e4ca1bca8aaf753c0e8941
Peter <piotr.major@remotecraftsmen.com>
2021-07-06 11:07:42 +0200

    fixed prettier issues

--- app/models/index.js
+++ app/models/index.js
@@ -1,73 +1,75 @@
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const config = require("../../config");
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const config = require('../../config');
 const basename = path.basename(__filename);
 const db = {};
 
 const Op = Sequelize.Op;
 const operatorsAliases = {
-  $eq: Op.eq,
-  $ne: Op.ne,
-  $gte: Op.gte,
-  $gt: Op.gt,
-  $lte: Op.lte,
-  $lt: Op.lt,
-  $not: Op.not,
-  $in: Op.in,
-  $notIn: Op.notIn,
-  $is: Op.is,
-  $like: Op.like,
-  $notLike: Op.notLike,
-  $iLike: Op.iLike,
-  $notILike: Op.notILike,
-  $regexp: Op.regexp,
-  $notRegexp: Op.notRegexp,
-  $iRegexp: Op.iRegexp,
-  $notIRegexp: Op.notIRegexp,
-  $between: Op.between,
-  $notBetween: Op.notBetween,
-  $overlap: Op.overlap,
-  $contains: Op.contains,
-  $contained: Op.contained,
-  $adjacent: Op.adjacent,
-  $strictLeft: Op.strictLeft,
-  $strictRight: Op.strictRight,
-  $noExtendRight: Op.noExtendRight,
-  $noExtendLeft: Op.noExtendLeft,
-  $and: Op.and,
-  $or: Op.or,
-  $any: Op.any,
-  $all: Op.all,
-  $values: Op.values,
-  $col: Op.col,
+    $eq: Op.eq,
+    $ne: Op.ne,
+    $gte: Op.gte,
+    $gt: Op.gt,
+    $lte: Op.lte,
+    $lt: Op.lt,
+    $not: Op.not,
+    $in: Op.in,
+    $notIn: Op.notIn,
+    $is: Op.is,
+    $like: Op.like,
+    $notLike: Op.notLike,
+    $iLike: Op.iLike,
+    $notILike: Op.notILike,
+    $regexp: Op.regexp,
+    $notRegexp: Op.notRegexp,
+    $iRegexp: Op.iRegexp,
+    $notIRegexp: Op.notIRegexp,
+    $between: Op.between,
+    $notBetween: Op.notBetween,
+    $overlap: Op.overlap,
+    $contains: Op.contains,
+    $contained: Op.contained,
+    $adjacent: Op.adjacent,
+    $strictLeft: Op.strictLeft,
+    $strictRight: Op.strictRight,
+    $noExtendRight: Op.noExtendRight,
+    $noExtendLeft: Op.noExtendLeft,
+    $and: Op.and,
+    $or: Op.or,
+    $any: Op.any,
+    $all: Op.all,
+    $values: Op.values,
+    $col: Op.col
 };
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
-    ...config,
-    operatorsAliases,
-  });
+    sequelize = new Sequelize(process.env[config.use_env_variable], {
+        ...config,
+        operatorsAliases
+    });
 } else {
-  sequelize = new Sequelize({ ...config.db, operatorsAliases });
+    sequelize = new Sequelize({ ...config.db, operatorsAliases });
 }
 
 fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach((file) => {
-    const model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+    .filter(file => {
+        return (
+            file.indexOf('.') !== 0 &&
+            file !== basename &&
+            file.slice(-3) === '.js'
+        );
+    })
+    .forEach(file => {
+        const model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model;
+    });
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 /**
@@ -76,7 +78,7 @@ Object.keys(db).forEach((modelName) => {
  * See https://github.com/sequelize/sequelize/issues/1500
  */
 Sequelize.Validator.notNull = function (item) {
-  return !this.isNull(item);
+    return !this.isNull(item);
 };
 
 db.sequelize = sequelize;
--- config/index.js
+++ config/index.js
@@ -1,44 +1,44 @@
-require("dotenv").config({
-  path:
-    process.env.NODE_ENV === "test"
-      ? ".env.test"
-      : process.env.NODE_ENV === "production"
-      ? ".env.production"
-      : ".env",
+require('dotenv').config({
+    path:
+        process.env.NODE_ENV === 'test'
+            ? '.env.test'
+            : process.env.NODE_ENV === 'production'
+            ? '.env.production'
+            : '.env'
 });
 
 const env = (key, defaultValue = null) => process.env[key] || defaultValue;
-const isEnabled = (key) => env(key) && env(key) === "true";
+const isEnabled = key => env(key) && env(key) === 'true';
 
 module.exports = {
-  enableDocs: isEnabled(env("ENABLEDOCS")),
-  db: {
-    url: `${env("DATABASE_DIALECT")}://${env("DATABASE_USERNAME")}:${env(
-      "DATABASE_PASSWORD"
-    )}@${env("DATABASE_HOST", "localhost")}:${env("DATABASE_PORT")}/${env(
-      "DATABASE_NAME"
-    )}`,
-    username: env("DATABASE_USERNAME"),
-    password: env("DATABASE_PASSWORD"),
-    database: env("DATABASE_NAME"),
-    logging: isEnabled(env("DATABASE_LOGGING")),
-    host: env("DATABASE_HOST"),
-    port: env("DATABASE_PORT"),
-    dialect: env("DATABASE_DIALECT"),
-  },
-  mail: {
-    sendgridApiKey: env("MAIL_SENDGRID_API_KEY"),
-    host: env("MAIL_HOST"),
-    port: env("MAIL_PORT"),
-    secure: isEnabled(env("MAIL_SECURE")),
-    auth: {
-      user: env("MAIL_AUTH_USER"),
-      pass: env("MAIL_AUTH_PASS"),
+    enableDocs: isEnabled(env('ENABLEDOCS')),
+    db: {
+        url: `${env('DATABASE_DIALECT')}://${env('DATABASE_USERNAME')}:${env(
+            'DATABASE_PASSWORD'
+        )}@${env('DATABASE_HOST', 'localhost')}:${env('DATABASE_PORT')}/${env(
+            'DATABASE_NAME'
+        )}`,
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        database: env('DATABASE_NAME'),
+        logging: isEnabled(env('DATABASE_LOGGING')),
+        host: env('DATABASE_HOST'),
+        port: env('DATABASE_PORT'),
+        dialect: env('DATABASE_DIALECT')
     },
-  },
-  jwt: {
-    secret: env("JWT_SECRET"),
-  },
-  frontendUrls: env("FRONTEND_URLS").split(" "),
-  noReplyAddress: env("NO_REPLY_ADDRESS"),
+    mail: {
+        sendgridApiKey: env('MAIL_SENDGRID_API_KEY'),
+        host: env('MAIL_HOST'),
+        port: env('MAIL_PORT'),
+        secure: isEnabled(env('MAIL_SECURE')),
+        auth: {
+            user: env('MAIL_AUTH_USER'),
+            pass: env('MAIL_AUTH_PASS')
+        }
+    },
+    jwt: {
+        secret: env('JWT_SECRET')
+    },
+    frontendUrls: env('FRONTEND_URLS').split(' '),
+    noReplyAddress: env('NO_REPLY_ADDRESS')
 };
