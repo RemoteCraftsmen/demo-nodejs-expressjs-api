34327304853a3b9ffc36b28de1954aeaf8b7fbb8
Peter <piotr.major@remotecraftsmen.com>
2021-08-02 18:01:06 +0200

    fixed test descriptions

--- test/controllers/Auth/checkPasswordResetController.js
+++ test/controllers/Auth/checkPasswordResetController.js
@@ -18,72 +18,74 @@ const passwordResetTokenGeneratorHandler = di.get(
     'services.passwordResetTokenGeneratorHandler'
 );
 
-describe('Change-password', () => {
-    before(async () => {
-        await truncateDatabase();
-        userData = userFactory.generate();
-        userData.passwordResetToken =
-            await passwordResetTokenGeneratorHandler.handle();
+describe('Auth', () => {
+    describe('POST /auth/reset-password/:token', () => {
+        before(async () => {
+            await truncateDatabase();
+            userData = userFactory.generate();
+            userData.passwordResetToken =
+                await passwordResetTokenGeneratorHandler.handle();
 
-        userData.passwordResetTokenExpiresAt = dayjs().add(2, 'hour');
-        user = await userFactory.create(userData);
-    });
+            userData.passwordResetTokenExpiresAt = dayjs().add(2, 'hour');
+            user = await userFactory.create(userData);
+        });
 
-    it('returns NO_CONTENT sending valid data as NOT LOGGED', async () => {
-        const password = 'password123';
-        const passwordConfirmation = password;
-        const { email } = user;
+        it('returns NO_CONTENT sending valid data as NOT LOGGED', async () => {
+            const password = 'password123';
+            const passwordConfirmation = password;
+            const { email } = user;
 
-        const response = await request
-            .post(`/auth/reset-password/${user.passwordResetToken}`)
-            .send({
-                password,
-                passwordConfirmation
+            const response = await request
+                .post(`/auth/reset-password/${user.passwordResetToken}`)
+                .send({
+                    password,
+                    passwordConfirmation
+                });
+
+            const updatedUser = await userRepository.getByEmail(email);
+            expect(updatedUser.passwordResetToken).to.equal(undefined);
+
+            expect(response.status).to.equal(StatusCodes.NO_CONTENT);
+
+            const loginResponse = await request
+                .post('/auth/login')
+                .send({ email, password });
+
+            expect(loginResponse.status).to.equal(StatusCodes.OK);
+        });
+
+        it('returns BAD REQUEST when password and password repeat are not equal as NOT LOGGED', async () => {
+            const { body, status } = await request
+                .post(`/auth/reset-password/${user.passwordResetToken}`)
+                .send({
+                    password: 'password123',
+                    passwordConfirmation: '456password'
+                });
+
+            expect(body.errors).to.deep.include({
+                message: 'Password confirmation must match password.',
+                param: 'passwordConfirmation'
             });
 
-        const updatedUser = await userRepository.getByEmail(email);
-        expect(updatedUser.passwordResetToken).to.equal(undefined);
+            expect(status).to.equal(StatusCodes.BAD_REQUEST);
+        });
 
-        expect(response.status).to.equal(StatusCodes.NO_CONTENT);
+        it('returns BAD REQUEST when body section is empty as NOT LOGGED', async () => {
+            const { body, status } = await request.post(
+                `/auth/reset-password/${user.passwordResetToken}`
+            );
 
-        const loginResponse = await request
-            .post('/auth/login')
-            .send({ email, password });
-
-        expect(loginResponse.status).to.equal(StatusCodes.OK);
-    });
-
-    it('returns BAD REQUEST when password and password repeat are not equal as NOT LOGGED', async () => {
-        const { body, status } = await request
-            .post(`/auth/reset-password/${user.passwordResetToken}`)
-            .send({
-                password: 'password123',
-                passwordConfirmation: '456password'
+            expect(body.errors).to.deep.include({
+                message: 'Password is required.',
+                param: 'password'
             });
 
-        expect(body.errors).to.deep.include({
-            message: 'Password confirmation must match password.',
-            param: 'passwordConfirmation'
+            expect(body.errors).to.deep.include({
+                message: 'Password confirmation is required.',
+                param: 'passwordConfirmation'
+            });
+
+            expect(status).to.equal(StatusCodes.BAD_REQUEST);
         });
-
-        expect(status).to.equal(StatusCodes.BAD_REQUEST);
-    });
-
-    it('returns BAD REQUEST when body section is empty as NOT LOGGED', async () => {
-        const { body, status } = await request.post(
-            `/auth/reset-password/${user.passwordResetToken}`
-        );
-
-        expect(body.errors).to.deep.include({
-            message: 'Password is required.',
-            param: 'password'
-        });
-
-        expect(body.errors).to.deep.include({
-            message: 'Password confirmation is required.',
-            param: 'passwordConfirmation'
-        });
-
-        expect(status).to.equal(StatusCodes.BAD_REQUEST);
     });
 });
--- test/controllers/Auth/requestPasswordResetController.js
+++ test/controllers/Auth/requestPasswordResetController.js
@@ -12,51 +12,53 @@ let userData;
 const di = app.get('di');
 const userRepository = di.get('repositories.user');
 
-describe('Reset-password', () => {
-    before(async () => {
-        await truncateDatabase();
+describe('Auth', () => {
+    describe('POST /auth/reset-password/', () => {
+        before(async () => {
+            await truncateDatabase();
 
-        userData = userFactory.generate();
-        user = await userFactory.create(userData);
-    });
+            userData = userFactory.generate();
+            user = await userFactory.create(userData);
+        });
 
-    it('returns NO_CONTENT sending valid data as NOT LOGGED', async () => {
-        const { email } = userData;
-        const { status } = await request
-            .post('/auth/reset-password/')
-            .send({ email });
+        it('returns NO_CONTENT sending valid data as NOT LOGGED', async () => {
+            const { email } = userData;
+            const { status } = await request
+                .post('/auth/reset-password/')
+                .send({ email });
 
-        const updatedUser = await userRepository.getByEmail(email);
+            const updatedUser = await userRepository.getByEmail(email);
 
-        expect(updatedUser).to.have.property('passwordResetToken');
+            expect(updatedUser).to.have.property('passwordResetToken');
 
-        expect(status).to.equal(StatusCodes.NO_CONTENT);
-    });
+            expect(status).to.equal(StatusCodes.NO_CONTENT);
+        });
 
-    it('returns NO_CONTENT sending invalid data as NOT LOGGED ', async () => {
-        const email = 'fakemail@example.com';
+        it('returns NO_CONTENT sending invalid data as NOT LOGGED ', async () => {
+            const email = 'fakemail@example.com';
 
-        const { status } = await request
-            .post('/auth/reset-password/')
-            .send({ email });
+            const { status } = await request
+                .post('/auth/reset-password/')
+                .send({ email });
 
-        expect(status).to.equal(StatusCodes.NO_CONTENT);
-    });
+            expect(status).to.equal(StatusCodes.NO_CONTENT);
+        });
 
-    describe('POST /reset-password', () => {
-        it('returns BAD REQUEST when body section is empty as NOT LOGGED', async () => {
-            const { body, status } = await request.post(
-                `/auth/reset-password/`
-            );
+        describe('POST /reset-password', () => {
+            it('returns BAD REQUEST when body section is empty as NOT LOGGED', async () => {
+                const { body, status } = await request.post(
+                    `/auth/reset-password/`
+                );
 
-            expect(body).to.have.property('errors');
+                expect(body).to.have.property('errors');
 
-            expect(body.errors).to.deep.include({
-                message: 'Email is required.',
-                param: 'email'
+                expect(body.errors).to.deep.include({
+                    message: 'Email is required.',
+                    param: 'email'
+                });
+
+                expect(status).to.equal(StatusCodes.BAD_REQUEST);
             });
-
-            expect(status).to.equal(StatusCodes.BAD_REQUEST);
         });
     });
 });
